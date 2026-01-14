b7867d68de523e7b2594e159128160942a198716
simplyasutosh <simplyasutosh@gmail.com>
2026-01-14 23:10:19 +0530

    t

--- app/src/main/java/com/construction/expense/data/local/mappers/CategoryMapper.kt
+++ app/src/main/java/com/construction/expense/data/local/mappers/CategoryMapper.kt
@@ -2,46 +2,51 @@ package com.construction.expense.data.local.mappers
 
 import com.construction.expense.data.local.entities.CategoryEntity
 import com.construction.expense.domain.model.Category
+import javax.inject.Inject
+import javax.inject.Singleton
 
 /**
- * Extension functions to convert between CategoryEntity and Category domain model.
+ * Mapper to convert between CategoryEntity and Category domain model.
  */
+@Singleton
+class CategoryMapper @Inject constructor() {
 
-/**
- * Convert CategoryEntity to Category domain model
- */
-fun CategoryEntity.toDomain(): Category {
-    return Category(
-        id = id,
-        name = name,
-        icon = icon,
-        color = color,
-        hasRoomTracking = hasRoomTracking,
-        sortOrder = sortOrder,
-        typicalBudgetPercent = typicalBudgetPercent,
-        isCustom = isCustom
-    )
-}
+    /**
+     * Convert CategoryEntity to Category domain model
+     */
+    fun toDomain(entity: CategoryEntity): Category {
+        return Category(
+            id = entity.id,
+            name = entity.name,
+            icon = entity.icon,
+            color = entity.color,
+            hasRoomTracking = entity.hasRoomTracking,
+            sortOrder = entity.sortOrder,
+            typicalBudgetPercent = entity.typicalBudgetPercent,
+            isCustom = entity.isCustom
+        )
+    }
 
-/**
- * Convert Category domain model to CategoryEntity
- */
-fun Category.toEntity(): CategoryEntity {
-    return CategoryEntity(
-        id = id,
-        name = name,
-        icon = icon,
-        color = color,
-        hasRoomTracking = hasRoomTracking,
-        sortOrder = sortOrder,
-        typicalBudgetPercent = typicalBudgetPercent,
-        isCustom = isCustom
-    )
-}
+    /**
+     * Convert Category domain model to CategoryEntity
+     */
+    fun toEntity(category: Category): CategoryEntity {
+        return CategoryEntity(
+            id = category.id,
+            name = category.name,
+            icon = category.icon,
+            color = category.color,
+            hasRoomTracking = category.hasRoomTracking,
+            sortOrder = category.sortOrder,
+            typicalBudgetPercent = category.typicalBudgetPercent,
+            isCustom = category.isCustom
+        )
+    }
 
-/**
- * Convert list of CategoryEntity to list of Category
- */
-fun List<CategoryEntity>.toDomainList(): List<Category> {
-    return map { it.toDomain() }
+    /**
+     * Convert list of CategoryEntity to list of Category
+     */
+    fun toDomainList(entities: List<CategoryEntity>): List<Category> {
+        return entities.map { toDomain(it) }
+    }
 }
--- app/src/main/java/com/construction/expense/data/local/mappers/SubCategoryMapper.kt
+++ app/src/main/java/com/construction/expense/data/local/mappers/SubCategoryMapper.kt
@@ -2,31 +2,45 @@ package com.construction.expense.data.local.mappers
 
 import com.construction.expense.data.local.entities.SubCategoryEntity
 import com.construction.expense.domain.model.SubCategory
+import javax.inject.Inject
+import javax.inject.Singleton
 
 /**
- * Extension functions to convert between SubCategoryEntity and SubCategory domain model.
+ * Mapper to convert between SubCategoryEntity and SubCategory domain model.
  */
+@Singleton
+class SubCategoryMapper @Inject constructor() {
 
-fun SubCategoryEntity.toDomain(): SubCategory {
-    return SubCategory(
-        id = id,
-        categoryId = categoryId,
-        name = name,
-        sortOrder = sortOrder,
-        isCustom = isCustom
-    )
-}
+    /**
+     * Convert SubCategoryEntity to SubCategory domain model
+     */
+    fun toDomain(entity: SubCategoryEntity): SubCategory {
+        return SubCategory(
+            id = entity.id,
+            categoryId = entity.categoryId,
+            name = entity.name,
+            sortOrder = entity.sortOrder,
+            isCustom = entity.isCustom
+        )
+    }
 
-fun SubCategory.toEntity(): SubCategoryEntity {
-    return SubCategoryEntity(
-        id = id,
-        categoryId = categoryId,
-        name = name,
-        sortOrder = sortOrder,
-        isCustom = isCustom
-    )
-}
+    /**
+     * Convert SubCategory domain model to SubCategoryEntity
+     */
+    fun toEntity(subCategory: SubCategory): SubCategoryEntity {
+        return SubCategoryEntity(
+            id = subCategory.id,
+            categoryId = subCategory.categoryId,
+            name = subCategory.name,
+            sortOrder = subCategory.sortOrder,
+            isCustom = subCategory.isCustom
+        )
+    }
 
-fun List<SubCategoryEntity>.toDomainList(): List<SubCategory> {
-    return map { it.toDomain() }
+    /**
+     * Convert list of SubCategoryEntity to list of SubCategory
+     */
+    fun toDomainList(entities: List<SubCategoryEntity>): List<SubCategory> {
+        return entities.map { toDomain(it) }
+    }
 }
